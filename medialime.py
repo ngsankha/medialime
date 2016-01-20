62608676b27a2faf5ed443a43d6de271e087bd72
Sankha Narayan Guria <sankha93@gmail.com>
2016-01-20 09:48:14 +0530

    allow to set mplayer path

--- medialime.py
+++ medialime.py
@@ -15,15 +15,18 @@ paused = False
 preferences = sublime.load_settings("Preferences.sublime-settings")
 
 def show_menu():
-  global music_library, library_index, songs
+  global preferences, music_library, library_index, songs
+  preferences = sublime.load_settings("Preferences.sublime-settings")
   music_library = str(preferences.get("music_library"))
-  inbuilt = [
-    ["Set Music Library", "Current Directory: " + music_library],
-  ]
+  inbuilt = []
   if (paused):
     inbuilt.append(["Play", "Now playing: " + current_playing])
   else:
     inbuilt.append(["Pause", "Now playing: " + current_playing])
+  inbuilt += [
+    ["Set Music Library", "Current Directory: " + music_library],
+    ["Set mplayer path", str(preferences.get("mplayer_path", "mplayer"))]
+  ]
   library_index = index_files(music_library)
   songs = list(library_index.keys())
   listing = inbuilt + songs
@@ -36,26 +39,35 @@ def update_music_library(new_location):
   preferences.set("music_library", new_location)
   sublime.save_settings("Preferences.sublime-settings")
 
+def update_mplayer_path(new_path):
+  preferences.set("mplayer_path", new_path)
+  sublime.save_settings("Preferences.sublime-settings")
+
 def selected_option(index):
   global current_playing, mplayer_process, paused
-  if (index < 2):
-    if (index == 1 and mplayer_process != -1 and mplayer_process.poll() is None):
+  if (index < 3):
+    if (index == 0 and mplayer_process != -1 and mplayer_process.poll() is None):
       if (paused):
         paused = False
       else:
         paused = True
       mplayer_process.stdin.write(b"pause\n")
       mplayer_process.stdin.flush()
-    else:
+    elif (index == 1):
       sublime.active_window().show_input_panel("Music library", music_library, update_music_library, None, None)
+    else:
+      sublime.active_window().show_input_panel("mplayer_path", preferences.get("mplayer_path", "mplayer"), update_mplayer_path, None, None)
   else:
-    if (mplayer_process != -1 and mplayer_process.poll() is not None):
+    if (mplayer_process != -1 and mplayer_process.poll() is None):
       mplayer_process.kill()
-    index -= 2
-    current_playing = songs[index]
-    mplayer_process = subprocess.Popen(["/usr/local/bin/mplayer", "-slave", library_index[current_playing]], stdin=subprocess.PIPE)
-    paused = False
-    sublime.status_message("Now playing: " + current_playing)
+    index -= 3
+    try:
+      mplayer_process = subprocess.Popen([preferences.get("mplayer_path", "mplayer"), "-slave", library_index[songs[index]]], stdin=subprocess.PIPE)
+      current_playing = songs[index]
+      paused = False
+      sublime.status_message("Now playing: " + current_playing)
+    except Exception:
+      pass
 
 def index_files(music_library):
   library_index = {}
